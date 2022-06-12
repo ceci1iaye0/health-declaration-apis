59aceaa293422e86c51557863695302de8a1a8b1
Cecilia <ceciliayeo.dev@gmail.com>
2022-06-12 16:39:48 +0800

    allow cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 
 import healthRouter from "./routes";
@@ -9,6 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+app.use(cors());
 
 /**
  * Routers
